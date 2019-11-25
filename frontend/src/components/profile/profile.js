b53c38d377b53a6f2b08ec67af74db6196f488e8
Stanton Huang <stantonhuang11@gmail.com>
2019-11-24 20:56:26 -0800

    just in case

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import "./profile.css";
 
 const Profile = () => {
     const [currentUser, setCurrentUser] = useState({});
@@ -9,12 +10,19 @@ const Profile = () => {
             .then(res => setCurrentUser(res.data))
     }, [])
 
-   
+    const getPKMN = () => {
+        if (currentUser.pokemon) {
+            return (<div>{currentUser.pokemon.length} / 809</div>)
+        }
+    }
+    
     if (currentUser) {
         return (
-            <div>
+            <div className="Profile">
                 <h2>Welcome {currentUser.handle}</h2>
+                {getPKMN()}
             </div>
+            
         );
     } else {
         return (
